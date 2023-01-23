6f1d4d914ef42a8af5f6787e5094dd639c8cc725
vhnegrisoli <victorhugonegrisoli.ccs@hotmail.com>
2023-01-23 16:02:49 -0300

    Fix python script

--- build.py
+++ build.py
@@ -1,24 +1,16 @@
 import os
 import threading
 
-stateless_path = '\stateless'
-stateful_path = '\stateful'
-full_path = os.getcwd()
-stateless_auth_api_path = stateless_path + '\stateless-auth-api'
-stateless_any_api_path = stateless_path + '\stateless-any-api'
-stateful_auth_api_path = stateful_path + '\stateful-auth-api'
-stateful_any_api_path = stateful_path + '\stateful-any-api'
 build_command = 'gradle build'
 threads = []
 
 
-def build_application(application_path):
-    threads.append(application_path)
-    build_path = full_path + application_path
-    print('Building application ' + application_path)
-    os.popen('cd ' + build_path + ' && ' + build_command).read()
-    print("Application " + application_path + " finished building!")
-    threads.remove(application_path)
+def build_application(app):
+    threads.append(app)
+    print('Building application ' + app)
+    os.system('cd ' + app + ' && ' + build_command)
+    print("Application " + app + " finished building!")
+    threads.remove(app)
 
 
 def docker_compose_up():
@@ -27,22 +19,23 @@ def docker_compose_up():
     print("Pipeline finished!")
 
 
+def run_all_builds():
+    threading.Thread(target=build_application,
+                     args={'stateless/stateless-auth-api'}).start()
+    threading.Thread(target=build_application,
+                     args={'stateless/stateless-any-api'}).start()
+    threading.Thread(target=build_application,
+                     args={'stateful/stateful-auth-api'}).start()
+    threading.Thread(target=build_application,
+                     args={'stateful/stateful-any-api'}).start()
+
+
 if __name__ == "__main__":
     print("Pipeline started!")
     print("Removing all containers.")
     os.system("docker-compose down")
     print("Starting to build applications!")
-
-    threading.Thread(target=build_application, args={
-                     stateless_auth_api_path}).start()
-    threading.Thread(target=build_application, args={
-                     stateless_any_api_path}).start()
-    threading.Thread(target=build_application, args={
-                     stateful_auth_api_path}).start()
-    threading.Thread(target=build_application, args={
-                     stateful_any_api_path}).start()
-
+    run_all_builds()
     while len(threads) > 0:
         pass
-
     threading.Thread(target=docker_compose_up).start()
